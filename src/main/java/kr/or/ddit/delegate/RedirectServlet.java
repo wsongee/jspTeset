aa172b5d9093d62857a8aff2f5fef27ce274b0ee
wsongee <thddl2236@gmail.com>
2020-10-05 09:08:34 +0900

    "maven  build cycle
    배포 자동화"
    배포 자동화
    배포 자동화
    "로깅 라이브러리
     . 로깅 레벨
     . 문자열 결합 주의"

--- src/main/java/kr/or/ddit/delegate/RedirectServlet.java
+++ src/main/java/kr/or/ddit/delegate/RedirectServlet.java
@@ -10,22 +10,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
 /**
  * Servlet implementation class RedirectServlet
  */
 @WebServlet("/redirectServlet")
 public class RedirectServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
- 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
+	//인자로 입력한 클래스의 패키지 정보를 확인 : kr.or.ddit.delegate.RedirectServlet
+//	private static Logger logger = LoggerFactory.getLogger(RedirectServlet.class); 
+	
+	private static final Logger logger = LoggerFactory.getLogger(RedirectServlet.class);
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//servlet은 응답을 만들어내는 역할이 아니라 요청받고 로직을 처리한 후 
 		//jsp에게 화면을 응답 생성을 위임한다.
 		
-		System.out.println("redirectServlet.doGet()");
-		
+//		System.out.println("redirectServlet.doGet()");
+		//문자열 결합만 조심하자(redirectServlet.doGet());
+		logger.debug("redirectServlet.doGet()"); //로깅레벨에따라
+//		logger.debug("redirectServlet{}", "doGet()"); //로깅레벨에따라
+	
 		//응답을 다른 jsp에게 위임하는 첫번째 방법 : redirect
 		//response 객체의 sendRedirect 메소드를 통해
 		//클라이언트에게 재 요청을 보낼 주소를 알려준다.
